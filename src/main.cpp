1bda14228cb547e68400f07334dd41d01dafbc66
Nicholas Guantai <nicholasguantai528@gmail.com>
2025-10-21 07:07:29 +0300

    type: executable files

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <filesystem>
+#include <cstdlib>
+#include <unistd.h>
 
 std::vector<std::string> split(const std::string &str)
 {
@@ -18,12 +21,44 @@ std::vector<std::string> split(const std::string &str)
   return tokens;
 }
 
+std::vector<std::string> getPATHDirectories(const char* path){
+  std::vector<std::string> directories;
+
+  if(path == nullptr){
+    return directories;
+  }
+
+  std::string pathStr(path);
+  size_t start = 0;
+
+  #ifdef _WIN32
+    char delimiter = ';';
+  #else
+    char delimiter = ':';
+  #endif
+
+  while(true){
+    size_t end = pathStr.find(delimiter, start);
+    if(end == std::string::npos){
+      directories.push_back(pathStr.substr(start));
+      break;
+    }
+
+    directories.push_back(pathStr.substr(start, end - start));
+    start = end + 1;
+  }
+
+  return directories;
+}
+
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::vector<std::string> built_in_command = {"echo", "exit", "type"};
+  const char* pathENV = std::getenv("PATH");
 
   while (true)
   {
@@ -65,7 +100,25 @@ int main() {
       }
       else
       {
-        std::cout << command_arg + ": not found" << std::endl;
+        auto pathDirs = getPATHDirectories(pathENV);
+        bool found = false;
+
+        for (const auto &dir : pathDirs)
+        {
+          std::string fullPath = dir + "/" + command_arg;
+          // check if file exists and is executable
+          if (access(fullPath.c_str(), X_OK) == 0)
+          {
+            std::cout << command_arg << " is " << fullPath << std::endl;
+            found = true;
+            break;
+          }
+        }
+
+        if(!found)
+        {
+          std::cout << command_arg + ": not found" << std::endl;
+        }
       }
     }
     else
